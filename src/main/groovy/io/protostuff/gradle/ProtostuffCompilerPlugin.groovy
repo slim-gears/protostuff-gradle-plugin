a72aa8dbbf01a98767195dff1ba0322efab48ca1
Ramon Wirsch <ramon.wirsch@stud.tu-darmstadt.de>
2014-11-24 23:12:55 +0100

    + check up-to-date\n+ make compileJava depend on us\n+ add gens to java.srcDir

--- src/main/groovy/io/protostuff/gradle/ProtostuffCompilerPlugin.groovy
+++ src/main/groovy/io/protostuff/gradle/ProtostuffCompilerPlugin.groovy
@@ -4,6 +4,7 @@ import io.protostuff.compiler.CachingProtoLoader
 import io.protostuff.compiler.CompilerMain
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.SourceSet
 
 /**
  * Protostuff Compiler Plugin for Gradle
@@ -13,7 +14,8 @@ import org.gradle.api.Project
 class ProtostuffCompilerPlugin implements Plugin<Project> {
 
     public static final String PROTOSTUFF_EXTENSION = 'protostuff'
-    public static final String GENERATE_SOURCE_TASK = 'generateSource'
+    public static final String GENERATE_SOURCE_TASK = 'generateProto'
+    public static final String CLEAN_GENERATE_TASK = 'cleanProto'
 
     @Override
     void apply(Project project) {
@@ -24,12 +26,36 @@ class ProtostuffCompilerPlugin implements Plugin<Project> {
             extensions.create(PROTOSTUFF_EXTENSION, ProtostuffExtension, modules)
         }
 
-        project.task(GENERATE_SOURCE_TASK) << {
-            ProtostuffExtension extension = project.protostuff;
-            CachingProtoLoader loader = extension.cacheProtos ? new CachingProtoLoader() : null;
-            extension.modules.each { m ->
-                m.setCachingProtoLoader(loader);
-                CompilerMain.compile(m);
+        project.afterEvaluate {
+            def sources = []
+            def outs = []
+
+            project.protostuff.modules.each { m ->
+                sources.add(m.source)
+                outs.add(m.outputDir)
+            }
+
+            project.tasks.create(GENERATE_SOURCE_TASK) {
+                description 'Generate java classes from all configured proto files'
+                ProtostuffExtension extension = project.protostuff;
+                inputs.file sources
+                outputs.dir outs
+
+                doLast {
+                    CachingProtoLoader loader = extension.cacheProtos ? new CachingProtoLoader() : null;
+                    extension.modules.each { m ->
+                        m.setCachingProtoLoader(loader);
+                        CompilerMain.compile(m);
+                    }
+                }
+
+            }
+
+            def genTask = project.tasks.getByName(GENERATE_SOURCE_TASK)
+            project.tasks.getByName('compileJava').dependsOn genTask
+
+            outs.each {
+                project.sourceSets.main.java.srcDir it
             }
         }
     }
